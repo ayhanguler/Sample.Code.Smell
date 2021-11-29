e6d4d5a2516473576b2f931bb9c3e8c30a8c28d5
Ayhan GÜLER <ayhanguler86@hotmail.com>
2021-11-29 11:18:32 +0300

    Add project files.

---
+++ Sample.Code.Smell/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Code.Smell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            p.Add("ürün1", 3, "0x000123", false);
+            p.Add("ürün2", 5, "0x000456", true);
+        }
+
+        class Product
+        {    
+            public void Add(string pName, int pAdet, string pColor, bool pStatus)
+            {
+                if (true)
+                {
+                    /*
+                     * bla bla bla
+                     */
+                }
+                else
+                {
+
+                }
+
+                if (true /*other conditions*/)
+                {
+                    /*
+                     * bla bla bla
+                     */
+                }
+            }
+        }
+
+    }
+}
